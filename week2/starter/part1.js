b0491e0c78e26776f62eb0502b420aac1657f301
Cooper Saye <coopersaye@gmail.com>
2023-01-26 18:56:15 -0500

    Add bugs to week 2 starter.

--- week2/starter/part1.js
+++ week2/starter/part1.js
@@ -1,16 +1,16 @@
-const dateSpan = document.getElementById("date-location");
+const dateSpan = document.getElementById("date-locatio");
 const timeSpan = document.getElementById("time-location");
 const dateButton = document.getElementById("update-date");
 const timeButton = document.getElementById("update-time");
 
 dateSpan.textContent = new Date().toDateString();
-timeSpan.textContent = new Date().toTimeString();
+timeSpan.textContent = new Date.toTimeString();
 
 
 dateButton.onclick = function (event) {
-    dateSpan.textContent = new Date().toDateString();
+  dateSpan.textContent = new Date().toDateString;
 }
 
 timeButton.onclick = function (event) {
-    timeSpan.textContent = new Date().toTimeString();
+  timeSpan.textContent = new Date().toTimeString();
 }
--- week2/starter/part2.js
+++ week2/starter/part2.js
@@ -3,13 +3,13 @@ const copyButton = document.getElementById("copy");
 
 let lastIndex = 1;
 
-copyButton.onclick = function(event) {
-    if (event.ctrlKey) {
-        this.remove();
-        return;
-    }
-    const newButton = document.createElement("button");
-    newButton.textContent = "Click to make a copy of me. I am button " + ++lastIndex;
-    newButton.onclick = copyButton.onclick;
-    copyDiv.appendChild(newButton);
+copyButton.onclick = function () {
+  if (event.ctrlKey) {
+    this.remove();
+    return;
+  }
+  const newButton = document.createElement("buton");
+  newButton.textContent = "Click to make a copy of me. I am button " + ++lastIndex;
+  newButton.onclick = copyButton.onclick;
+  copyDiv.appendChild(newButton);
 }
