9e0b6547d7397ff504443ac1f3d21e3b4b69b289
KindaOK <27974340+KindaOK@users.noreply.github.com>
2023-03-09 14:09:33 -0500

    Rename football to FootballTable
    Component name should match file name. Also changed the filters for
    the table slightly.

--- week6/complete/src/App.js
+++ week6/complete/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import FootballTable from "./football";
+import FootballTable from "./FootballTable";
 import footballData from "./football.json";
 
 function App() {
     return (
         <div className="App">
             <img src={logo} className="App-logo"/>
-            <FootballTable footballData={footballData.filter(data => data.year % 2)}/>
-            <FootballTable footballData={footballData.filter(data => data.year % 2)}/>
+            <FootballTable footballData={footballData}/>
+            <FootballTable footballData={footballData.filter(data => !(data.year % 5))}/>
         </div>
     );
 }
