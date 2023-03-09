6cbe07818e82f048c1e94a5297ad4a8bcce74245
KindaOK <27974340+KindaOK@users.noreply.github.com>
2023-03-09 14:02:03 -0500

    Add the logo back
    This will show them how to add images to an application.

--- week6/complete/src/App.js
+++ week6/complete/src/App.js
@@ -4,11 +4,13 @@ import FootballTable from "./football";
 import footballData from "./football.json";
 
 function App() {
-  return (
-    <div className="App">
-      <FootballTable footballData={footballData}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo"/>
+            <FootballTable footballData={footballData.filter(data => data.year % 2)}/>
+            <FootballTable footballData={footballData.filter(data => data.year % 2)}/>
+        </div>
+    );
 }
 
 export default App;
